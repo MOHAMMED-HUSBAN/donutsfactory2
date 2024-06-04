1d74918f4e94aa46c4e104f9f648b1a3dd87cd1d
mohammed-husban <22015295011@std.bau.edu.jo>
2024-06-04 17:25:29 +0300

    Dounts

---
+++ j2.js
@@ -0,0 +1,98 @@
+/////////////////////////////////////////////////////////////////////////////////////// ( 1 )
+
+console.log(-5*3);
+console.log("JavaScript " + 50  );
+console.log (17 % 5) ;
+console.log (5 % 17 )  ;
+console.log (5/10 )  ;
+console.log (4 === '4') ;
+console.log (4 != 5)  ;
+console.log (7 <= 8) ;
+console.log ("Hello" + 5);
+let x = 2.5;
+let result = Math.ceil(x) - Math.floor(x);
+console.log(result);
+j = 3;
+let result2 = Math.pow(j, 2);
+console.log(result2);
+
+// /////////////////////////////////////////////////////////////////////////////////////// ( 2 )
+
+// let number = prompt("This is the box prompt , now enter the number : ");
+// alert("This is an alert box and you entered : " + number);
+
+// /////////////////////////////////////////////////////////////////////////////////////// ( 3 )
+
+// let number1 =prompt("Please enter the first number :");
+// let number2 =prompt("Please enter the second number :");
+// if (number1 > number2){
+//     alert("The numbers in ascending order are: " + number1 + " , " + number2);
+// }
+// else if (number1 < number2) {
+//     alert("The numbers in ascending order are: " + number2 + " , " + number1);
+// }
+// else {
+//     alert("Both numbers are equal: " + number1);
+// }
+
+// /////////////////////////////////////////////////////////////////////////////////////// ( 4 )
+
+// let number1 =prompt("Please enter the first number :");
+// let number2 =prompt("Please enter the second number :");
+// if (number1 > number2){
+//      alert("The larger numbers : " + number1 );
+// }
+
+// else if (number1 < number2) {
+//     alert("The larger numbers : "  +number2 );
+// }
+// else {
+//     alert("Both numbers are equal: " + number1);
+// }
+
+// /////////////////////////////////////////////////////////////////////////////////////// ( 5 )
+
+// let number1 = parseFloat(prompt("Please enter the first number: "));
+// let number2 = parseFloat(prompt("Please enter the second number: "));
+// let sum = number1 + number2;
+// alert("The sum of the two numbers is: " + sum);
+
+// /////////////////////////////////////////////////////////////////////////////////////// ( 6 )
+
+// let number = prompt("Enter the number you want to convert to writing : ");
+
+// switch (parseInt(number)) {
+//   case 1:
+//     alert("ONE");
+//     break;
+//   case 2:
+//     alert("TWO");
+//     break;
+//   case 3:
+//     alert("THREE");
+//     break;
+//   case 4:
+//     alert("FOUR");
+//     break;
+//   case 5:
+//     alert("FIVE");
+//     break;
+//   case 6:
+//     alert("SIX");
+//     break;
+//   case 7:
+//     alert("SEVEN");
+//     break;
+//   case 8:
+//     alert("EIGHT");
+//     break;
+//   case 9:
+//     alert("NINE");
+//     break;
+//             default:
+//               alert("TRY AGAIN");
+// }
+
+
+
+
